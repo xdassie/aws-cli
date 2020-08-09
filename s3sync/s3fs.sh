3b58cc0eeec30516e3d524da033cdc635b203add
xdassie <50299712+xdassie@users.noreply.github.com>
2020-08-09 23:52:44 +0200

    Update s3fs.sh

--- s3sync/s3fs.sh
+++ s3sync/s3fs.sh
@@ -1,5 +1,4 @@
-. /root/.aws/CREDENTIALS
 export S3_ACL=${S3_ACL:-private}
 export AWSACCESSKEYID=${AWSACCESSKEYID:-$AWS_ACCESS_KEY_ID}
 export AWSSECRETACCESSKEY=${AWSSECRETACCESSKEY:-$AWS_SECRET_ACCESS_KEY}
-/usr/bin/s3fs ${S3_BUCKET} ${MOUNT_POINT} -o nosuid,nonempty,nodev,allow_other,default_acl=${S3_ACL},retries=5
+/usr/bin/s3fs ${S3_BUCKET} ${MOUNT_POINT}  -o nosuid,nonempty,nodev,allow_other,default_acl=${S3_ACL},retries=5,passwd_file=/root/.aws/credentials
