0f90eee5abf9e81a6e9cb5d6c1757fd30a924181
xdassie <50299712+xdassie@users.noreply.github.com>
2020-08-15 20:06:33 +0200

    Update s3sync.sh

--- s3sync/s3sync.sh
+++ s3sync/s3sync.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-s3fs --help
+AWS_ACCESS_KEY_ID=$(aws configure get my_profile.aws_access_key_id)
+AWS_SECRET_ACCESS_KEY=$(aws configure get my_profile.aws_secret_access_key)
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+s3fs $BUCKET[:/$BUCKETPATH] $MOUNTPOINT [options]
 encfs /encrypted /clear
 
