81db422a14c39dcf92b29ca8a7263b605a575a81
xdassie <50299712+xdassie@users.noreply.github.com>
2020-08-15 21:57:46 +0200

    Update s3sync.sh

--- s3sync/s3sync.sh
+++ s3sync/s3sync.sh
@@ -3,5 +3,5 @@ AWS_ACCESS_KEY_ID=$(aws configure get my_profile.aws_access_key_id)
 AWS_SECRET_ACCESS_KEY=$(aws configure get my_profile.aws_secret_access_key)
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
-s3fs -f --debug -o endpoint="af-south-1" -o url="https://s3.af-south-1.amazonaws.com" "$BUCKET:/$BUCKETPATH" /encrypted
+s3fs -o endpoint="af-south-1" -o url="https://s3.af-south-1.amazonaws.com" "$BUCKET:/$BUCKETPATH" /encrypted
 encfs -o password /encrypted /clear
