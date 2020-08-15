a279c07da1351a506e305b71be93fa3717e4c04c
xdassie <50299712+xdassie@users.noreply.github.com>
2020-08-15 23:42:15 +0200

    Update s3sync.sh

--- s3sync/s3sync.sh
+++ s3sync/s3sync.sh
@@ -4,6 +4,7 @@ AWS_SECRET_ACCESS_KEY=$(aws configure get my_profile.aws_secret_access_key)
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 mkdir -p /encrypted
-s3fs -d -d -o endpoint="af-south-1" -o url="https://s3.af-south-1.amazonaws.com" "$BUCKET:/$BUCKETPATH" /encrypted && \
-  encfs /encrypted /tmp/clear
+s3fs -d -d -o endpoint="af-south-1" -o url="https://s3.af-south-1.amazonaws.com" "$BUCKET:/$BUCKETPATH" /encrypted
+#&& \
+#  encfs /encrypted /tmp/clear
 #while true; do echo "syncing encrytped filesystem with s3"; sleep 60; done;
